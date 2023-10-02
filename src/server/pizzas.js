7f2c194d5e06e3591797d37336a97b5982b55ee9
Cristin <tianncristiann@gmail.com>
2023-10-02 14:10:26 +0300

    test async/await

--- src/server/pizzas.js
+++ src/server/pizzas.js
@@ -1,13 +1,24 @@
 import axios from 'axios';
 import { setNrPizzas } from '../redux/slices/filterSlice';
 
-export function fetchPizzas(urlDB, setPizzas, setIsLoading, currentPage, dispatch) {
-	axios.get(urlDB).then((res) => dispatch(setNrPizzas(res.data.length)));
+export async function fetchPizzas(urlDB, setPizzas, setIsLoading, currentPage, dispatch) {
+	try {
+		const res = await axios.get(urlDB);
+		dispatch(setNrPizzas(res.data.length));
+	} catch (error) {
+		console.error(error);
+	}
+
 	urlDB.searchParams.append('page', currentPage);
 	urlDB.searchParams.append('limit', 4);
-
-	axios.get(urlDB).then((res) => {
-		setPizzas(res.data);
-		setIsLoading(false);
-	});
+	axios
+		.get(urlDB)
+		.then((res) => {
+			setPizzas(res.data);
+			setIsLoading(false);
+		})
+		.catch((error) => {
+			setIsLoading(false);
+			console.error(error);
+		});
 }
